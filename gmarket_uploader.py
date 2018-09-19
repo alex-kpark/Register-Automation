ddb6deab0f7adb0906021bfd5e379e476a168d21
Alex Kyungho Park <kyungho.dev@gmail.com>
2018-09-19 22:44:27 +0900

    #2 Ebay uploader finished
    Ebay uploader finished

--- gmarket_uploader.py
+++ gmarket_uploader.py
@@ -2,7 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common import action_chains
+from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
 #For OSX
@@ -12,7 +12,6 @@ from time import sleep
 driver = webdriver.Chrome('C:/Users/PC/AppData/Local/Programs/Python/chromedriver.exe')
 driver.get('https://www.esmplus.com/Member/SignIn/LogOn?ReturnValue=-7')
 
-#login
 def login(id, pw):
 
     input_id = driver.find_element_by_xpath("//input[@id='Id']")
@@ -30,9 +29,7 @@ def login(id, pw):
     #iframe: element를 못잡으면 그 안에 iframe이 형성되어있을 가능성이 높다 or sleep을 주거나
     driver.get('http://www.esmplus.com/Home/Home#HTDM395')
     driver.switch_to_frame("ifm_TDM395")
-    
-    
-
+      
 def select_market():
     cancel_ac = driver.find_element_by_xpath("//input[@name='regi-nameiac'][@value='false']")
     cancel_gmk = driver.find_element_by_xpath("//input[@name='regi-namegmk'][@value='false']")
@@ -47,7 +44,6 @@ def select_market():
     else:
         pass
 
-#category : 브랜드 카테고리 항목을 잘 못잡음
 def category(cate_name):
     search_esm = driver.find_element_by_xpath("//input[@placeholder='ESM 카테고리명 검색']")
     search_esm.send_keys(cate_name)
@@ -59,20 +55,36 @@ def category(cate_name):
     esm_target.click()
     sleep(1)
 
+    press_keys = ActionChains(driver)
+
     auc_cates = driver.find_elements_by_xpath("//select[@style='width: 160px;']")
     auc_cates[0].find_element_by_xpath("//option[contains(text(), '가방/패션잡화')]").click()
-    auc_cates[1].find_element_by_xpath("//option[contains(text(), '여성가방')]").click()
-    auc_cates[2].find_element_by_xpath("//option[contains(text(), '토트백')]").click()
-    print('finished')
+    auc_cates[1].click()
+    press_keys.key_down(Keys.DOWN).perform()
+    press_keys.key_down(Keys.ENTER).perform()
+    sleep(2)
+    auc_cates[2].click()
+    press_keys.key_down(Keys.DOWN).perform()
+    press_keys.key_down(Keys.ENTER).perform()
+    sleep(3)    
+    
+    gmk_ops = driver.find_elements_by_xpath("//select[@style='width: 215px;']/option[@value='[object Object]']")
+    gmk_ops[3].click()
+    sleep(2)
 
-    gmk_cates = driver.find_elements_by_xpath("//select[@style='width: 215px;']")
-    print(1)
-    gmk_cates[0].find_element_by_xpath("//option[contains(text(), '브랜드 신발/가방')]").click()
-    print(2)
-    gmk_cates[1].find_element_by_xpath("//option[contains(text(), '여성가방')]").click()
-    print(3)
-    #driver.find_element_by_xpath("//select[@style='width: 214px;']/option[contains(text(), '토트백')]").click()
-    print(4)
+    gmk_cates = driver.find_elements_by_xpath("//select[@style='width: 215px;']")    
+    gmk_cates[1].click()
+    press_keys.key_down(Keys.DOWN).perform()
+    sleep(2)
+    press_keys.key_down(Keys.ENTER).perform()
+    sleep(1)
+
+    gmk_min = driver.find_element_by_xpath("//select[@style='width: 214px;']")
+    gmk_min.click()
+    press_keys.key_down(Keys.DOWN).perform()
+    sleep(2)
+    press_keys.key_down(Keys.ENTER).perform()
+    sleep(1)
 
 def product_info(name, promo, auc_price, gmk_price):
     prod_name = driver.find_element_by_xpath("//input[@id='txtGoodsNameSearch']")
@@ -128,15 +140,7 @@ def duration(num):
     elif num == 30:
         duration_bar.send_keys(Keys.UP)
         duration_bar.send_keys(Keys.UP)  
-        '''
-        elif num == 15:
-            print(15)
-            i = 1
-            while i == 2:
-                duration_bar.send_keys(Keys.UP)
-                i = i + 1
-            print('15success')
-        '''
+
     elif num == 15:
         duration_bar.send_keys(Keys.UP)
         duration_bar.send_keys(Keys.UP)
@@ -210,16 +214,18 @@ def exposure_info(deliver_date, deliver_method, product_detail):
             save_end = driver.find_element_by_xpath("//button[@class='ee-button ee-button-positive']")
             save_end.click()
 
-            #driver.close()
-
+            window_before = driver.window_handles[0]
+            window_after = driver.window_handles[1]
+            driver.switch_to_window(window_before)
+            
         except Exception as e:
             print('fail')
-    
 
-    '''
+    driver.switch_to_frame("ifm_TDM395")
+    
     #상품정보공시 입력 - 카테고리에 따라 value 입력
-    product_type = driver.find_element_by_xpath("//select[@name='ddlOfficialNotiGroup']/option[@value='3']")
-    product_type.click()
+    product_type = driver.find_element_by_xpath("//select[@id='ddlOfficialNotiGroup']/option[@value='3']")
+    product_type.click()   
 
     #발송정책 - 옥션만
     if deliver_date == True:
@@ -244,38 +250,73 @@ def exposure_info(deliver_date, deliver_method, product_detail):
     else:
         direct_deliv = driver.find_element_by_xpath("//input[@id='rdoCommonDeliveryWayOPTSEL3']")
         direct_deliv.click()
-    '''
-    
 
+def legal_info(prod_type, expect_deliv, fabric, color, size, importer, nation, caution, assurance, respons):
+    areas = driver.find_elements_by_xpath("//textarea[@name='noti_item_value']")
+    areas[0].send_keys(prod_type)
+    areas[1].send_keys(expect_deliv)
+    areas[2].send_keys(fabric)
+    areas[3].send_keys(color)
+    areas[4].send_keys(size)
+    areas[5].send_keys(importer)
+    areas[6].send_keys(nation)
+    areas[7].send_keys(caution)
+    areas[8].send_keys(assurance)
+    areas[9].send_keys(respons)
+    print('success')
+
+def finish():
+    driver.find_element_by_xpath("//li[@class='menu_item3']").click()
+    driver.find_element_by_xpath("//li[@class='menu_item4']").click()
+    finish_btn = driver.find_element_by_xpath("//img[@src='http://pics.esmplus.com/front/sell/syi_save.gif']")
+    finish_btn.click()
+    
+    finish_attempt = driver.window_handles[2]
+    driver.switch_to_window(finish_attempt)
+    driver.find_element_by_xpath("//label[@id='lbConfirmForGoodsImage']").click()
+    driver.find_element_by_xpath("//label[@id='lblConfirmForGoodsName']").click()
+    driver.find_element_by_xpath("//label[@id='lblConfirmForSellerDiscount']").click()
+    driver.find_element_by_xpath("//label[@id='lblConfirmForGoodsPrice']").click()
+
+    final_btn = driver.find_element_by_xpath("//img[@src='http://pics.esmplus.com//front/sell/btn_final_confirm.gif']")
+    final_btn.click()
+
+
+#Activate from here
 login('#id', '#pw')
 
 auc = True
 gmk = True
 
-exposure_info(False, 0, '상품의 자세한 정보를 이곳에 입력해주세요')
-
-#category('여성가방')
-
-'''
 select_market()
 
-            
+category('여성가방')
+
+product_info('제품명',
+            '프로모션',
+            10000,
+            15000)
+
 gmk_prod_info('English Please',
             '你好',
             'エンジニア')
 
 duration(60)
 
-product_info('제품 이름을 입력해주십시오',
-            '프로모션 이름을 입력해주십시오',
-            10000,
-            15000)
-
 #discount : m - money, p - percentage, n - none / l - limited duration, u - unlimited
-extra_info(['m', 1000, 'l', '2018-09-15', '2018-10-20'])
+extra_info(['m', 1000, 'l', '2018-09-22', '2018-10-20'])
 
+exposure_info(False, 0, '상품의 자세한 정보를 이곳에 입력해주세요')
 
-#True/False for 배송일 / 0,1,2 for 발송방법 
+legal_info('종류를 입력해주세요',
+            '기간을 입력해주세요',
+            '소재를 입력해주세요',
+            '색상을 입력해주세요',
+            '크기를 입력해주세요',
+            '제조사를 입력해주세요',
+            '생산지를 입력해주세요',
+            '조심히 다루어주세요',
+            '고객의 과실 시 환불 불가',
+            '010-0000-0000')
 
-
-'''
+finish()
