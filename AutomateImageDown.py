8de0f02cf82170bb81d0b7e3f9ae82a547c49c87
Alex Kyungho Park <kyungho.dev@gmail.com>
2018-10-01 16:21:28 +0900

    #14 Updated for 청약
    청약철회 가능하도록 수정

--- AutomateImageDown.py
+++ AutomateImageDown.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 import xlrd
 from collections import OrderedDict
 import json
@@ -13,6 +15,17 @@ import os
 import sys
 from time import sleep
 
+#엑셀 돌면서 URL타다가
 wb = xlrd.open_workbook('upload_target.xlsx')
 sh = wb.sheet_by_index(0)
 
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+for rownum in range(1, sh.nrows):
+    row_values = sh.row_values(rownum)
+    driver.get(row_values[24])
+
+    #images Directory는 원하는 곳으로 지정 가능
+    url = driver.find_element_by_xpath("//img[@style='-webkit-user-select: none;cursor: zoom-in;']").get_attribute("src")
+    save_name = 'images/image_'+str(rownum)+'.jpg'
+    urllib.request.urlretrieve(url, save_name)
